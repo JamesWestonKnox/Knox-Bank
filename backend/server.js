245a5ccaed63069397616bfcbddb83426fc52597
James Knox <jamesknox361@gmail.com>
2025-10-08 10:50:50 +0200

    shap

--- backend/server.js
+++ backend/server.js
@@ -14,14 +14,12 @@ connectDB();
 
 const app = express();
 
-// Middleware
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/staff", staffRoutes);
