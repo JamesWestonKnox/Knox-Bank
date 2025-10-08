90a2c3a55c119502bf426c5bbd0c3834d44d0ba1
James Knox <jamesknox361@gmail.com>
2025-10-08 11:32:54 +0200

    Password hash field

--- backend/models/Customer.js
+++ backend/models/Customer.js
@@ -4,7 +4,7 @@ const CustomerSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   idNumber: { type: String, required: true },
   accountNumber: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  passwordHash: { type: String, required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model("Customer", CustomerSchema);
