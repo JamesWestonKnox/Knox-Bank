9bc28548d3603949659ec92c7c6028ad4f902304
James Knox <jamesknox361@gmail.com>
2025-10-08 11:01:11 +0200

    Fixed routing

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,8 @@ import cookieParser from "cookie-parser";
 import rateLimit from "express-rate-limit";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
-import paymentRoutes from "./routes/paymentRoutes.js";
-import staffRoutes from "./routes/staffRoutes.js";
+import transactionRoutes from "./routes/transactionRoutes.js";
+import customerRoutes from "./routes/customerRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -21,8 +21,8 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
 
 app.use("/api/auth", authRoutes);
-app.use("/api/payments", paymentRoutes);
-app.use("/api/staff", staffRoutes);
+app.use("/api/transaction", transactionRoutes);
+app.use("/api/customer", customerRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
