619be1c312f8529b944f64e42121c878d85cf864
st10275468 <st10275468@vcconnect.edu.za>
2025-10-08 10:50:39 +0200

    Created Customer and Transaction attributes

---
+++ backend/models/Customer.js
@@ -0,0 +1,10 @@
+const Customer = {
+  id: Number,
+  fullName: String,
+  idNumber: String,
+  accountNumber: String,
+  passwordHash: String,
+  createdAt: Date,
+};
+
+module.exports = Customer;
---
+++ backend/models/Transaction.js
@@ -0,0 +1,13 @@
+const Transaction = {
+  id: Number,
+  customerId: Number,
+  amount: Number,
+  currency: String,
+  provider: String,
+  payeeAccount: String,
+  swift: String,
+  status: "PENDING",
+  createdAt: Date,
+};
+
+module.exports = Transaction;
