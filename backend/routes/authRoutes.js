6214a6400089df363d374a0d07db27d7fa872674
James Knox <jamesknox361@gmail.com>
2025-10-09 18:32:34 +0200

    Added brute force prevention to login

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { login } from "../controllers/authController.js";
+import ExpressBrute from "express-brute";
 
 const router = express.Router();
 
-router.post("/login", login);
+var store = new ExpressBrute.MemoryStore();
+var bruteforce = new ExpressBrute(store);
+
+router.post("/login",bruteforce.prevent, login);
 
 router.get("/", (req, res) => {
   res.send("Auth route working!");
