b1791b0cbce9665985ff55170cf0b2ccf01e9c47
st10275468 <st10275468@vcconnect.edu.za>
2025-10-09 17:44:28 +0200

    Fixing

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,3 +8,17 @@ const API = axios.create({
 export const registerCustomer = async (data) => {
   return await API.post("customer/register", data);
 };
+
+export async function loginCustomer(credentials) {
+  const res = await fetch("http://localhost:5000/api/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(credentials),
+  });
+
+  if (!res.ok) {
+    throw new Error("Login failed");
+  }
+
+  return await res.json(); 
+}
