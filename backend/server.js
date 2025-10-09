223b5513f4b04616870d62f21c49a466ad1f02a8
James Knox <jamesknox361@gmail.com>
2025-10-09 18:03:57 +0200

    https

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const options = {
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://localhost:5173", credentials: true }));
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
 
 app.use("/api/customer", customerRoutes);
