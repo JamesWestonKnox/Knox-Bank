b8fd8f774833bbb16aab07f57bc4e5e40ce3e38a
James Knox <jamesknox361@gmail.com>
2025-10-10 10:18:44 +0200

    Edited jwt token expiry

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,16 +4,20 @@ dotenv.config();
 
 const authMiddleware = (req, res, next) => {
   try {
-    const authHeader = req.headers["authorization"];
-    if (!authHeader) return res.status(401).json({ error: "Access denied, token missing" });
 
-    const token = authHeader.split(" ")[1];
-    if (!token) return res.status(401).json({ error: "Access denied, token missing" });
+    const token = req.cookies?.token || (req.headers["authorization"]?.startsWith("Bearer ")
+    ? req.headers["authorization"].split(" ")[1] : null);
+
+    if (!token)
+      return res.status(401).json({ error: "Access denied, token missing"});
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
+    if (err.name === "TokenExpiredError") {
+      return res.status(401).json({ error: "Token expired, please log in again" });
+    }
     res.status(401).json({ error: "Invalid token" });
   }
 };
