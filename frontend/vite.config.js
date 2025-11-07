5e36c685e1ec1e6461fa8d29bf8c97defc516c37
James Knox <jamesknox361@gmail.com>
2025-11-07 13:47:59 +0200

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import fs from "fs";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-     https: {
+    https: {
       key: fs.readFileSync("../backend/keys/privatekey.pem"),
       cert: fs.readFileSync("../backend/keys/certificate.pem"),
     },
